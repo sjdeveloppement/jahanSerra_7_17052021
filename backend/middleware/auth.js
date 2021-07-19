7ca4096b32f57ef23c80c359faae9bf6fb3f3539
Serra jahan <jahan.serra@gmail.com>
2021-07-19 18:34:45 +0200

    modification auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];// extract token from headers authorization
+        console.log(token);
         const decodedToken = jwt.verify(token, process.env.JWT_SECRET_TOKEN);// verify the token with .env
         const userId = decodedToken.userID; //extract user_id from token
         //console.log(decodedToken.userID);
@@ -12,8 +13,13 @@ module.exports = (req, res, next) => {
         //console.log(decodedToken);
         //console.log(userId);
         //console.log(token);
+        
+        if (req.body.userId && req.body.userId !== userId) {
+            throw 'Invalid user ID';
+        } else {
+            console.log('token authentifié')
             next();
-    
+        }
     } catch (error) {
         
         res.status(401).json({ error: error | 'Requête non authentifiée !' });
